fe869b85ba3677a5639cdd543a0050182ab5849f
Abed-Hasouneh <a.r.hasouneh@gmail.com>
2022-06-26 14:44:27 +0300

    adding type script to monster-rolodex

--- src/components/card-list/CardList.tsx
+++ src/components/card-list/CardList.tsx
@@ -1,9 +1,12 @@
+import { Monster } from '../../App';
 import Card from '../card/Card';
 import './card-list.css';
 
+type CardListProps = {
+  monsters: Monster[];
+};
 
-
-const CardList = ({ monsters }) => (
+const CardList = ({ monsters }: CardListProps) => (
   <div className='card-list'>
     {monsters.map((monster) => {
       return <Card key={monster.id} monster={monster} />;
